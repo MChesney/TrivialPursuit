6d100a824448205bb77cbcdec92c28973df7255f
parke261 <parke261@msu.edu>
2013-04-28 11:24:05 -0400

    df

--- BigDraw/src/edu/msu/cse/trivialpursuit/bigdraw/MainActivity.java
+++ BigDraw/src/edu/msu/cse/trivialpursuit/bigdraw/MainActivity.java
@@ -1,7 +1,13 @@
 package edu.msu.cse.trivialpursuit.bigdraw;
 
+import java.io.IOException;
+
+import android.media.AudioManager;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.SeekBar;
@@ -23,14 +29,36 @@ public class MainActivity extends Activity {
 	 * The draw toggle
 	 */
 	private ToggleButton drawToggle; 
+	
+	/**
+	 * The media recorder to keep track of volume
+	 */
+	MediaRecorder recorder;
+	
+	/**
+	 * Amplitude of microphone input
+	 */
+	int amp;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
 		drawView = (DrawView) this.findViewById(R.id.drawView);
-		
+		recorder = new MediaRecorder();
+		setUpMediaRecorder();
+		Thread thread = new Thread(new Runnable(){
+
+			runOnUiThread(new Runnable() {
+	            @Override
+	            public void run() {
+	                risultato.setText("" + recorder.getMaxAmplitude());
+	            }
+	        });
+			
+		}
+		);
+		thread.start();
 		drawToggle = (ToggleButton) findViewById(R.id.drawToggle);
 		thicknessBar = (SeekBar) findViewById(R.id.thicknessBar);
 			
@@ -65,4 +93,30 @@ public class MainActivity extends Activity {
 		drawView.setEditable(!drawToggle.isChecked());
 	}
 
+	public void setUpMediaRecorder()
+	{
+		 recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+		 recorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+		 recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+		 recorder.setOutputFile("/dev/null");
+		 try {
+			recorder.prepare();
+	 	 } catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		 } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		 }
+	     recorder.start();   // Recording is now started
+	}
+	
+	public void unsetMediaRecorder()
+	{
+		 recorder.stop();
+		 recorder.reset();
+  // You can reuse the object by going back to setAudioSource() step
+		 recorder.release(); // Now the object cannot be reused
+		 
+	}
 }
